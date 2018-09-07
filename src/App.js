4f2a95612c21850f3da3ad82d3670bb3060447c9
Mantaroh Yoshinaga <mantaroh@gmail.com>
2018-09-07 10:52:14 +0900

    Remove the unnecessary code, and add frame test code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      createNum: 30,
+      label: "Log here",
+    };
+
+    this.createFrame = this.createFrame.bind(this);
+    this.setCreateNum = this.setCreateNum.bind(this);
+    this.keyHandle = this.keyHandle.bind(this);
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error);
+    console.log(info);
+  }
+
+  createFrame() {
+    this.setState({
+      label: "Wait for finishing load the frame."
+    });
+
+    let count = 0;
+    let intervalId = setInterval(() => {
+      let frame = document.createElement("iframe");
+      document.body.appendChild(frame);
+      if (count++ > this.state.createNum) {
+        clearInterval(intervalId);
+        this.setState({
+          label: "Go! Click the frames button!."
+        });
+      }
+    }, 50);
+  }
+
+  setCreateNum(event) {
+    this.setState({createNum: event.target.value});
+  }
+
+  keyHandle(event) {
+    const code = event.key;
+    switch (code) {
+      case "Enter":
+        this.createFrame();
+        break;
+      default:
+        break;
+    }
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    const children = [];
+
+    // Log Element
+    children.push(
+      React.createElement(
+        "div",
+        {
+          key: "logLabel",
+          ref: "logLabel",
+        },
+        this.state.label
+      )
     );
+
+    // Input Element
+    children.push(
+      React.createElement(
+        "input",
+        {
+          type: "number",
+          ref: "tryNumber",
+          key: "tryNumber",
+          value: this.state.createNum,
+          onChange: this.setCreateNum,
+          onKeyDown: this.keyHandle,
+        }
+      )
+    );
+
+    // Button Element
+    children.push(
+      React.createElement(
+        "button",
+        {
+          key: "GoButton",
+          onClick: this.createFrame,
+        },
+        "Go"
+      )
+    );
+    return (React.createElement("div", {}, children));
   }
 }
 
